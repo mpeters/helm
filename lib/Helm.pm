9c0a3b79b4e570552576d4053df887d8a82de331
Michael Peters <mpeters@plusthree.com>
2010-09-24 13:52:35 -0400

    add local locking

--- lib/Helm.pm
+++ lib/Helm.pm
@@ -6,19 +6,25 @@ use Moose::Util::TypeConstraints qw(enum);
 use URI;
 use namespace::autoclean;
 use Try::Tiny;
-use File::Spec::Functions qw(catdir);
+use File::Spec::Functions qw(catdir catfile tmpdir);
 use File::HomeDir;
 use Net::OpenSSH;
 use Carp qw(croak);
+use Fcntl qw(:flock);
+use File::Basename qw(basename);
 
 our $VERION = 0.1;
 
 enum NOTIFY_LEVEL => qw(debug info warn error fatal);
-has task          => (is => 'ro', writer => '_task',       required => 1);
-has extra_options => (is => 'ro', isa    => 'HashRef',     default  => sub { {} });
-has config_uri    => (is => 'ro', writer => '_config_uri', isa      => 'Str');
-has config        => (is => 'ro', writer => '_config',     isa      => 'Helm::Conf');
-has sudo          => (is => 'ro', writer => '_sudo',       isa      => 'Str');
+enum LOCK_TYPE    => qw(none local remote both);
+has task              => (is => 'ro', writer => '_task',              required => 1);
+has extra_options     => (is => 'ro', isa    => 'HashRef',            default  => sub { {} });
+has config_uri        => (is => 'ro', writer => '_config_uri',        isa      => 'Str');
+has config            => (is => 'ro', writer => '_config',            isa      => 'Helm::Conf');
+has sudo              => (is => 'ro', writer => '_sudo',              isa      => 'Str');
+has lock_type         => (is => 'ro', writer => '_lock_type',         isa      => 'LOCK_TYPE');
+has local_lock_handle => (is => 'ro', writer => '_local_lock_handle', isa      => 'FileHandle');
+has sleep             => (is => 'ro', writer => '_sleep',             isa      => 'Num');
 has servers    => (
     is      => 'ro',
     writer  => '_servers',
@@ -100,6 +106,10 @@ sub steer {
     my $task_obj = $task_class->new($self);
     $task_obj->validate();
 
+    # make sure have a local lock if we need it
+    croak("Cannot obtain a local helm lock. Is another helm process running?")
+      if ($self->lock_type eq 'local' || $self->lock_type eq 'both') && !$self->_get_local_lock;
+
     # make sure we can connect to all of the servers
     my %ssh_connections;
     my @servers = @{$self->servers};
@@ -124,7 +134,10 @@ sub steer {
         );
         warn "$thin_line\n";
         warn "\n" unless $i == $#servers;
+        sleep($self->sleep) if $self->sleep;
     }
+
+    # release the local lock
 }
 
 sub load_configuration {
@@ -142,6 +155,30 @@ sub load_configuration {
     croak("Unknown config type: $scheme. Couldn't load $loader_class: $@") if $@;
 
     return $loader_class->load($uri);
+}
+
+sub _get_local_lock {
+    my $self = shift;
+    # lock the file so nothing else can run at the same time
+    my $lock_handle;
+    my $lock_file = $self->_local_lock_file();
+    open($lock_handle, '>', $lock_file) or croak("Can't open $lock_file for locking: $!");
+    if (flock($lock_handle, LOCK_EX | LOCK_NB)) {
+        $self->_local_lock_handle($lock_handle);
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+sub _release_local_lock {
+    my $self = shift;
+    close($self->_local_lock_handle) if $self->_local_lock_handle;
+}
+
+sub _local_lock_file {
+    my $self = shift;
+    return catfile(tmpdir(), 'helm.lock');
 }
 
 __PACKAGE__->meta->make_immutable;
